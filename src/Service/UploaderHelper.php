b702f5779c0c6b74ba9fac520caad15b9ac4de22
Jurij Leshchuk <ddevdev609@gmail.com>
2023-10-25 08:33:02 +0300

    Using the Filesystem

--- src/Service/UploaderHelper.php
+++ src/Service/UploaderHelper.php
@@ -3,6 +3,7 @@
 namespace App\Service;
 
 use Gedmo\Sluggable\Util\Urlizer;
+use League\Flysystem\FilesystemInterface;
 use Symfony\Component\Asset\Context\RequestStackContext;
 use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
@@ -10,27 +11,26 @@ use Symfony\Component\HttpFoundation\File\UploadedFile;
 class UploaderHelper
 {
     const ARTICLE_IMAGE = 'article_image';
-    /**
-     * @var string
-     */
-    private $uploadsPath;
+
     /**
      * @var RequestStackContext
      */
     private $requestStackContext;
+    /**
+     * @var FilesystemInterface
+     */
+    private $filesystem;
 
     public function __construct(
-        string $uploadsPath,
+        FilesystemInterface $publicUploadFilesystem,
         RequestStackContext $requestStackContext
     ) {
-        $this->uploadsPath = $uploadsPath;
         $this->requestStackContext = $requestStackContext;
+        $this->publicUploadFilesystem = $publicUploadFilesystem;
     }
 
     public function uploadArticleImage(File $file): string
     {
-        $destination = $this->uploadsPath . '/' . self::ARTICLE_IMAGE;
-
         if ($file instanceof UploadedFile) {
             $originalFileName = $file->getClientOriginalName();
         } else {
@@ -39,9 +39,9 @@ class UploaderHelper
 
         $newFileName = Urlizer::urlize(pathinfo($originalFileName, PATHINFO_FILENAME)) . '-'.\uniqid() . '.' . $file->guessExtension();
 
-        $file->move(
-            $destination,
-            $newFileName
+        $this->publicUploadFilesystem->write(
+            self::ARTICLE_IMAGE . '/' . $newFileName,
+            file_get_contents($file->getPathname())
         );
 
         return $newFileName;
